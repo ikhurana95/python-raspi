53424363d3836176fd5910d86a898431bc850147
Ishan Khurana <ishankhurana@eduroam-int-dhcp-97-232-239.ucl.ac.uk>
2019-02-23 14:48:18 +0000

    setup files added

---
+++ setup.sh
@@ -0,0 +1,18 @@
+## Install Jupyter
+sudo apt-get install -y python-dev
+sudo pip3 install --upgrade pip
+sudo pip3 install jupyter
+sudo apt-get install -y ttf-bitstream-vera
+sudo ipython3 kernelspec install-self
+
+sudo apt-get install sense-hat
+
+# Install senseGraphics
+pip install senseGraphics
+
+# Install Pan Tilt
+
+curl https://get.pimoroni.com/pantilthat | bash
+
+# Install senslib with
+# pip3 install senslib
