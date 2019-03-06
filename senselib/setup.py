42832304610aba8e546c14e23320b4ad4ec2b069
Harjeet Kalsi <33220635+harjeetkalsi@users.noreply.github.com>
2019-03-06 16:11:39 +0000

    Create setup.py

---
+++ senselib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='senselib',
+      version='0.1',
+      description='Drawing functions for Raspberry Pi Sense HAT',
+      author='Ishan Khurana',
+      author_email='ishan.khurana.15@ucl.ac.uk',
+      license='MIT',
+      packages=['senselib'],
+      install_requires=['numpy', 'senseGraphics'],
+      )
