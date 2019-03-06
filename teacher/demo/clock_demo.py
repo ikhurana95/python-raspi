dc09252b14d6dcbf01833b6cd00716ab4b09bc50
Harjeet Kalsi <33220635+harjeetkalsi@users.noreply.github.com>
2019-03-06 17:13:09 +0000

    Update clock_demo.py

--- teacher/demo/clock_demo.py
+++ teacher/demo/clock_demo.py
@@ -8,5 +8,4 @@ while True:
 	time_now = datetime.now().time() 
 	time_now_string = str(time_now) 
 	time_now_string_truncated = time_now_string[0:5] 
-	s.show_message(time_now_string_truncated, 0.05, text_c 
-	olour=[255,0,0]) 
+	s.show_message(time_now_string_truncated, 0.05, text_colour=[255,0,0]) 
