d19acd9bbcec6e9b6f7cf5baa40bedea1c748356
ishank95 <ishan.khurana.14@ucl.ac.uk>
2019-03-07 12:51:50 +0000

    required pkgs updated

--- senselib/setup.py
+++ senselib/setup.py
@@ -3,9 +3,7 @@ from setuptools import setup
 setup(name='senselib',
       version='0.1',
       description='Drawing functions for Raspberry Pi Sense HAT',
-      author='Ishan Khurana',
-      author_email='ishan.khurana.15@ucl.ac.uk',
+      author='Lukas Kikuchi',
       license='MIT',
       packages=['senselib'],
-      install_requires=['numpy', 'senseGraphics'],
       )
